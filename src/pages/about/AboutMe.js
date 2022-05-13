dd86eafd41f9888f12f961507e0ecec3e648518f
bykop <byko.dev@gmail.com>
2022-05-13 19:33:18 +0200

    initial commit

---
+++ src/pages/about/AboutMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import photo from "../../assets/photo12.jpg";
+const AboutMe = () => {
+
+    return (
+        <div className="about_me_section" id="about">
+            <div className="image_container">
+            </div>
+        </div>
+    )
+
+}
+export default AboutMe;
